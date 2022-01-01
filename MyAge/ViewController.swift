ea638deccbe494036a2291242341bf12c77e635e
İhlas Mert <ihlas1mert@gmail.com>
2022-01-01 11:04:18 +0300

    ComletedMyAgeApp

--- MyAge/ViewController.swift
+++ MyAge/ViewController.swift
@@ -7,13 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    
+    @IBOutlet var textYearofBirth: UITextField!
+    @IBOutlet var lashowAge: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    
+    @IBAction func BuFindDage(_ sender: Any) {
+        
+        
+        
+        GetAge()
+    }
+    
+    
+    func GetAge(){
+        
+        
+        
+        let yearOfAge = Int(textYearofBirth.text!)
+        
+        let date = Date()
+        let calendar = Calendar.current
+        let year = calendar.component(.year, from: date)
+        
+        
+        let personAge = 2022 - yearOfAge!
+        lashowAge.text = "your age is \(personAge)"
+        
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    func textFieldShouldReturn( textField: UITextField) -> Bool{
+        GetAge()
+        self.view.endEditing(true)
+        return true
+        
+    }
+    
+    
 }
 
